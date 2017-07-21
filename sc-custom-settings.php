2d15cbef874d3a9edd746da7bf8b0e309389937f
Surfing-Chef <surfing-chef@surfing-chef.com>
2017-07-20 19:26:27 -0700

    Initial commit

---
+++ sc-custom-settings.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * Plugin Name: SC Custom Settings
+ * Plugin URI: https://github.com/Surfing-Chef/sc-custom-settings
+ * Description: Adds a place for custom settings within the WordPress Admin Dashboaard Settings
+ * Version: 1.0
+ * Author: Surfing-Chef
+ * Author URI: https://github.com/Surfing-Chef
+ * License: GPL3
+ * License URI: https://www.gnu.org/licenses/gpl-3.0.html
+ * Text Domain: sc-custom-settings
+*/
