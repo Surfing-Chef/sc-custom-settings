f222b7361bf7129ddc39a4739166d677f871d6ad
Surfing-Chef <surfing-chef@surfing-chef.com>
2017-07-22 07:03:49 -0700

    Experimented with various boilerplates and tutorials

--- README.md
+++ README.md
@@ -2,9 +2,11 @@
 [![GitHub version 1.0](https://badge.fury.io/gh/Surfing-Chef%2Fsc-custom-settings.svg)](https://badge.fury.io/gh/Surfing-Chef%2Fsc-custom-settings)  [![license](https://img.shields.io/github/license/mashape/apistatus.svg)](https://github.com/Surfing-Chef/sc-custom-settings)
 
 Helpful Development Links:  
+- [Creating Custom WordPress Administration Pages](https://code.tutsplus.com/tutorials/creating-custom-admin-pages-in-wordpress-1--cms-26829)
 - [The Complete Guide to the WordPress Settings API](https://code.tutsplus.com/series/the-complete-guide-to-the-wordpress-settings-api--cms-624)  
 - [Understanding and Working with The WordPress Options Table](https://code.tutsplus.com/tutorials/understanding-and-working-with-the-wordpress-options-table--cms-21119)  
 - [How to Make a WordPress Admin Options Page (Without Using the Settings API)](https://wpshout.com/wordpress-options-page/)
 - [WordPress Codex](https://codex.wordpress.org/)
 - [WordPress Developer](https://developer.wordpress.org/)  
-- [WordPress Plugins, Menus and Settings](https://developer.wordpress.org/plugins/)
+- [WordPress Plugins, Menus and Settings](https://developer.wordpress.org/plugins/)  
+- [WordPress Developer: Custom Settings Page Example](https://developer.wordpress.org/plugins/settings/custom-settings-page/)  
--- sc-custom-settings.php
+++ sc-custom-settings.php
@@ -9,165 +9,151 @@
  * License: MIT
  * License URI: https://www.gnu.org/licenses/gpl-3.0.html
  * Text Domain: sc-custom-settings
-
+ *
 */
-// add_action( 'admin_init', 'sc_plugin_admin_init' );
-// add_action( 'admin_menu', 'sc_create_menu_page' );
-//
-// /**
-//  * Register our stylesheet.
-//  */
-// function sc_plugin_admin_init() {
-//
-//   //Register stylesheet
-//   wp_register_style( 'scPluginStylesheet', plugins_url( 'styles.css', __FILE__ ) );
-// }
 
 /**
- * Register our plugin page and hook stylesheet loading.
+ * @internal never define functions inside callbacks.
+ * these functions could be run multiple times; this would result in a fatal error.
  */
-// function sc_create_menu_page() {
-//     $page = add_submenu_page(
-//   		'options-general.php',
-//   		'SC Custom Settings',
-//   		'SC Custom Settings',
-//   		'administrator',
-//   		'sc-custom-settings',
-//   		'sc_custom_settings_callback'
-//   	);
-//
-//     add_action( "admin_print_styles-{$page}", 'sc_plugin_admin_styles' );
-// }
 
 /**
- * Enqueue our stylesheet.
+ * custom option and settings
  */
-// function sc_plugin_admin_styles() {
-//     wp_enqueue_style( 'scPluginStylesheet' );
-// }
+function sc_settings_init() {
+ // register a new setting for "sc" page
+ register_setting( 'sc', 'sc_options' );
 
-/* ------------------------------------------------------------------------ *
- * Setting Registration
- * ------------------------------------------------------------------------ */
+ // register a new section in the "sc" page
+ add_settings_section(
+ 'sc_section_developers',
+ __( 'APIs', 'sc' ),
+ 'sc_section_developers_cb',
+ 'sc'
+ );
+
+ // register a new field in the "sc_section_developers" section, inside the "sc" page
+ add_settings_field(
+ 'sc_field_googleMaps', // as of WP 4.6 this value is used only internally
+ // use $args' label_for to populate the id inside the callback
+ __( 'Google Maps API', 'sc' ),
+ 'sc_field_googleMaps_cb',
+ 'sc',
+ 'sc_section_developers',
+ [
+ 'label_for' => 'sc_field_googleMaps',
+ 'class' => 'sc_row',
+ 'sc_custom_data' => 'custom',
+ ]
+ );
+}
 
 /**
- * Initializes the theme options page by registering the Sections,
- * Fields, and Settings.
- *
- * This function is registered with the 'admin_init' hook.
+ * register our sc_settings_init to the admin_init action hook
  */
-add_action('admin_init', 'sc_initialize_custom_settings');
-function sc_initialize_custom_settings() {
-  // Enqueue our stylesheet
-  wp_register_style( 'scPluginStylesheet', plugins_url( 'styles.css', __FILE__ ) );
-  wp_enqueue_style( 'scPluginStylesheet' );
-
-    // Register a section. This is necessary since all future options must belong to one.
-    add_settings_section(
-        'sc_settings_section',         // ID used to identify this section and with which to register options
-        'SC Custom Settings',          // Title to be displayed on the administration page
-        'sc_custom_settings_callback', // Callback used to render the description of the section
-        'general'                      // Page on which to add this section of options
-    );
-
-    // Next, we will introduce the fields for toggling the visibility of content elements.
-    add_settings_field(
-        'sc_google_api',                      // ID used to identify the field throughout the theme
-        'Google API',                           // The label to the left of the option interface element
-        'sc_toggle_header_callback',   // The name of the function responsible for rendering the option interface
-        'general',                          // The page on which this option will be displayed
-        'sc_settings_section',         // The name of the section to which this field belongs
-        array(                              // The array of arguments to pass to the callback. In this case, just a description.
-            'Activate this setting to display the header.'
-        )
-    );
-
-    // Finally, we register the fields with WordPress
-    register_setting(
-        'general',
-        'sc_google_api'
-    );
-
-} // end sc_initialize_custom_settings
-
-/* ------------------------------------------------------------------------ *
- * Section Callbacks
- * ------------------------------------------------------------------------ */
+add_action( 'admin_init', 'sc_settings_init' );
 
 /**
- * This function provides a simple description for the General Options page.
- *
- * It is called from the 'sc_initialize_custom_settings' function by being passed as a parameter
- * in the add_settings_section function.
+ * custom option and settings:
+ * callback functions
  */
-function sc_custom_settings_callback() {
-    echo '<p>Select which areas of content you wish to display.</p>';
-} // end sc_custom_settings_callback
 
-/* ------------------------------------------------------------------------ *
- * Field Callbacks
- * ------------------------------------------------------------------------ */
+// developers section cb
+
+// section callbacks can accept an $args parameter, which is an array.
+// $args have the following keys defined: title, id, callback.
+// the values are defined at the add_settings_section() function.
+function sc_section_developers_cb( $args ) {
+ ?>
+ <p id="<?php echo esc_attr( $args['id'] ); ?>"><?php esc_html_e( 'Developer APIs used.', 'sc' ); ?></p>
+ <?php
+}
+
+// pill field cb
+
+// field callbacks can accept an $args parameter, which is an array.
+// $args is defined at the add_settings_field() function.
+// wordpress has magic interaction with the following keys: label_for, class.
+// the "label_for" key value is used for the "for" attribute of the <label>.
+// the "class" key value is used for the "class" attribute of the <tr> containing the field.
+// you can add custom key value pairs to be used inside your callbacks.
+function sc_field_googleMaps_cb( $args ) {
+ // get the value of the setting we've registered with register_setting()
+ $options = get_option( 'sc_options' );
+
+ // output the field
+ ?>
+ <select id="<?php echo esc_attr( $args['label_for'] ); ?>"
+ data-custom="<?php echo esc_attr( $args['sc_custom_data'] ); ?>"
+ name="sc_options[<?php echo esc_attr( $args['label_for'] ); ?>]"
+ >
+ <option value="red" <?php echo isset( $options[ $args['label_for'] ] ) ? ( selected( $options[ $args['label_for'] ], 'red', false ) ) : ( '' ); ?>>
+ <?php esc_html_e( 'red pill', 'sc' ); ?>
+ </option>
+ <option value="blue" <?php echo isset( $options[ $args['label_for'] ] ) ? ( selected( $options[ $args['label_for'] ], 'blue', false ) ) : ( '' ); ?>>
+ <?php esc_html_e( 'blue pill', 'sc' ); ?>
+ </option>
+ </select>
+ <p class="description">
+ <?php esc_html_e( 'You take the red pill and you stay in Wonderland and I show you how deep the rabbit-hole goes.', 'sc' ); ?>
+ </p>
+ <?php
+}
 
 /**
- * This function renders the interface elements for toggling the visibility of the header element.
- *
- * It accepts an array of arguments and expects the first element in the array to be the description
- * to be displayed next to the checkbox.
+ * top level menu
  */
-function sc_toggle_header_callback($args) {
+function sc_options_page() {
+ // add top level menu page
+ add_menu_page(
+ 'SC Theme Settings',
+ 'Theme Settings',
+ 'manage_options',
+ 'sc',
+ 'sc_options_page_html'
+ );
+}
 
-    // Note the ID and the name attribute of the element match that of the ID in the call to add_settings_field
-    $html = '<input type="checkbox" id="show_header" name="show_header" value="1" ' . checked(1, get_option('show_header'), false) . '/>';
+/**
+ * register our sc_options_page to the admin_menu action hook
+ */
+add_action( 'admin_menu', 'sc_options_page' );
 
-    // Here, we will take the first argument of the array and add it to a label next to the checkbox
-    $html .= '<label for="show_header"> '  . $args[0] . '</label>';
+/**
+ * top level menu:
+ * callback functions
+ */
+function sc_options_page_html() {
+ // check user capabilities
+ if ( ! current_user_can( 'manage_options' ) ) {
+ return;
+ }
 
-    echo $html;
+ // add error/update messages
 
-} // end sc_toggle_header_callback
+ // check if the user have submitted the settings
+ // wordpress will add the "settings-updated" $_GET parameter to the url
+ if ( isset( $_GET['settings-updated'] ) ) {
+ // add settings saved message with the class of "updated"
+ add_settings_error( 'sc_messages', 'sc_message', __( 'Settings Saved', 'sc' ), 'updated' );
+ }
 
-function sc_create_menu_page() {
-
-    add_menu_page(
-        'SC Custom Settings',    // The title to be displayed on the corresponding page for this menu
-        'SC Custom Settings',    // The text to be displayed for this actual menu item
-        'administrator',         // Which type of users can see this menu
-        'sc-custom-settings',    // The unique ID - that is, the slug - for this menu item
-        'sc_menu_page_display',  // The name of the function to call when rendering the menu for this page
-        ''
-    );
-
-    add_submenu_page(
-    		'options-general.php',        // Register this submenu with the menu defined above
-    		'SC Custom Settings',         // The text to the display in the browser when this menu item is active
-    		'SC Custom Settings',         // The text for this menu item
-    		'administrator',              // Which type of users can see this menu
-    		'sc-custom-settings',         // The unique ID - the slug - for this menu item
-    		'sc_custom_settings_display' // The function used to render the menu for this page to the screen
-  	);
-
-} // end sc_create_menu_page
-add_action('admin_menu', 'sc_create_menu_page');
-
-function sc_menu_page_display() {
-  // Create a header in the default WordPress 'wrap' container
-  $html = '<div class="wrap">';
-      // icon here
-      $html .= '<h2>Settings</h2>';
-  $html .= '</div>';
-
-  // Send the markup to the browser
-  echo $html;
-
-} // end sc_menu_page_display
-
-function sc_custom_settings_display() {
-  // Create a header in the default WordPress 'wrap' container
-    $html = '<div class="wrap">';
-        // icon here
-        $html .= '<h2>Sandbox Options</h2>';
-    $html .= '</div>';
-
-    // Send the markup to the browser
-    echo $html;
-} // end sc_custom_settings_display
+ // show error/update messages
+ settings_errors( 'sc_messages' );
+ ?>
+ <div class="wrap">
+ <h1><?php echo esc_html( get_admin_page_title() ); ?></h1>
+ <form action="options.php" method="post">
+ <?php
+ // output security fields for the registered setting "sc"
+ settings_fields( 'sc' );
+ // output setting sections and their fields
+ // (sections are registered for "sc", each field is registered to a specific section)
+ do_settings_sections( 'sc' );
+ // output save settings button
+ submit_button( 'Save Settings' );
+ ?>
+ </form>
+ </div>
+ <?php
+}
